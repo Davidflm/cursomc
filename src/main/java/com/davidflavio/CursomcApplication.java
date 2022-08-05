8ec2e51f1ff33915bc70096cd424796a82ae826f
David Flávio <davidflaviolm1982@gmail.com>
2022-08-05 10:14:55 -0300

    Created Estado and Cidade domain and Repositories

--- src/main/java/com/davidflavio/CursomcApplication.java
+++ src/main/java/com/davidflavio/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.davidflavio.domain.Categoria;
+import com.davidflavio.domain.Cidade;
+import com.davidflavio.domain.Estado;
 import com.davidflavio.domain.Produto;
 import com.davidflavio.repositories.CategoriaRepository;
+import com.davidflavio.repositories.CidadeRepository;
+import com.davidflavio.repositories.EstadoRepository;
 import com.davidflavio.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRespository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -41,8 +49,25 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		Estado est3 = new Estado(null,"Ceará");
+		
+		Cidade c1 = new Cidade(null,"Uberlandia", est1);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		Cidade c4 = new Cidade(null,"Fortaleza", est3);
+		Cidade c5 = new Cidade(null,"Pacatuba",est3);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		est3.getCidades().addAll(Arrays.asList(c4,c5));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRespository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2,est3));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5));
 		
 	}
 	
---
+++ src/main/java/com/davidflavio/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.davidflavio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.davidflavio.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
